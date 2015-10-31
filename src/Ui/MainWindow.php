8eab7319c13b85c74696dd0c819f4766f7e4e388
ondrejd <ondrejd@gmail.com>
2015-10-31 03:33:39 +0100

    Removed start/stop area and the third tab.

--- src/Ui/MainWindow.php
+++ src/Ui/MainWindow.php
@@ -53,70 +53,22 @@ class MainWindow extends \GtkWindow {
    * @return void
    */
   protected function setupUi() {
-    $mainBox = new \GtkVBox();
+    $main = new \GtkVBox();
 
-    $startStopArea = new \GtkHBox();
-
-    $vbox = new \GtkVBox();
-    $hbox = new \GtkVBox();
-
-    $label = new \GtkLabel();
-    $label->set_markup(
-        '<span>Create new activity:</span>'.
-        '<span color="darkgray" font="sans">[ACTIVITY_NAME][@PROJECT_NAME][#TAGS]</span>'.
-        ''
-    );
-    $label->set_single_line_mode(false);
-
-    $hbox->pack_start($label, false);
-    $vbox->pack_start($hbox, false);
-
-    $textBuffer = new \GtkTextBuffer();
-    $textView = new \GtkTextView();
-    $textBuffer->set_text('Hello World!');
-    $textView->set_buffer($textBuffer);
-    $textView->set_editable(true);
-    $vbox->pack_start($textView, true);
-
-    $startStopArea->pack_start($vbox, true);
-    $mainBox->pack_start($startStopArea, false);
-
-    // ==============================================================
+    // Main tabbox
     $notebook = new \GtkNotebook();
 
+    // 1. tab - activities
     $tab1 = new \GtkVBox();
-    //$tab1->pack_start(new \GtkLabel('This is the first tab'));
     new ActivitiesTreeview($tab1);
-    $notebook->append_page(
-        $tab1,
-        new \GtkLabel('Activities')
-    );
+    $notebook->append_page($tab1, new \GtkLabel('Activities'));
 
-    // Projects tab
+    // 2. tab - projects
     $tab2 = new \GtkVBox();
     new ProjectsTreeview($tab2);
-    $notebook->append_page(
-        $tab2,
-        new \GtkLabel('Projects')
-    );
+    $notebook->append_page($tab2, new \GtkLabel('Projects'));
 
-    //Create the third page, with an icon as label and
-    //some nested childs
-    $tab3 = new \GtkVBox();
-    $tab3->pack_start(new \GtkLabel('This is the third tab'));
-    $tab3->pack_start(new \GtkEntry(), false, false);
-    $tab3->pack_start(new \GtkButton('Test'), false, false);
-    $notebook->append_page(
-        $tab3,
-        \GtkImage::new_from_stock(
-            \Gtk::STOCK_ADD,
-            \Gtk::ICON_SIZE_MENU
-        )
-    );
-    $mainBox->pack_start($notebook, true);
-    // ==============================================================
-
-
-    $this->add($mainBox);
+    $main->pack_start($notebook, true);
+    $this->add($main);
   } // end setupUi()
 } // End of MainWindow
